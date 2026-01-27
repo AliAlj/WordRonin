bd9b6ce799bcc79e14f2796ba2cb3a59cb8f27bd
AliAlj <alimjahmi@gmail.com>
2026-01-27 00:49:01 -0500

    Initial Commit

---
+++ slicenspellTests/slicenspellTests.swift
@@ -0,0 +1,17 @@
+//
+//  slicenspellTests.swift
+//  slicenspellTests
+//
+//  Created by Ali Aljahmi on 1/27/26.
+//
+
+import Testing
+@testable import slicenspell
+
+struct slicenspellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
